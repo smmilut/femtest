8df993fc547f55af38fc68df4bbc88bfc5c8f469
Pil Smmilut <pil.smmilut@gmail.com>
2022-02-11 15:25:28 +0100

    add simple assert functions for arrays and objects

--- utils/femtest/test.js
+++ utils/femtest/test.js
@@ -95,17 +95,75 @@ export const assert = {
         }
         throw new AssertError(`No exception thrown.`);
     },
+    arraysEqual: function assert_arraysEqual(array1, array2, equality = function strictEquality(a, b) { return a === b; }) {
+        if (!Array.isArray(array1)) {
+            throw new AssertError(`"${array1}" !== "${array2}" because the first is not an Array.`);
+        }
+        if (!Array.isArray(array2)) {
+            throw new AssertError(`[${array1}] !== "${array2}" because the second is not an Array.`);
+        }
+        if (array1.length !== array2.length) {
+            throw new AssertError(`[${array1}] !== [${array2}] because they have different length.`);
+        }
+        if (!array1.every((val, index) => equality(val, array2[index]))) {
+            throw new AssertError(`[${array1}] !== [${array2}] because they have different elements.`);
+        }
+        return true;
+    },
+    /**
+     * Answer to : JSON.stringify(object1) === JSON.stringify(object2);
+     * note : the "order" of properties actually matters !
+     */
+    objectsJsonEqual: function assert_objectsJsonEqual(object1, object2) {
+        const json1 = JSON.stringify(object1);
+        const json2 = JSON.stringify(object2);
+        if (json1 === json2) {
+            return true;
+        } else {
+            throw new AssertError(`Objects JSON strings are not equal for ${json1} and ${json2}.`);
+        }
+    },
+    /**
+     * Compare two objects :
+     *  - does `object2` contain all the own properties of `object1` and are they all strictly equal ? 
+     *  - does `object1` contain all the own properties of `object2` and are they all strictly equal ? 
+     */
+    objectsShallowStrictEqual: function assert_objectsShallowStrictEqual(object1, object2) {
+        assert._rightObjectHasAllLeftPropertiesStrictEqual(object1, object2);
+        assert._rightObjectHasAllLeftPropertiesStrictEqual(object2, object1);
+        return true;
+    },
+    /**
+     * (internal) does `object2` contain all the own properties of `object1` and are they all strictly equal ?
+     */
+    _rightObjectHasAllLeftPropertiesStrictEqual: function assert_rightObjectHasAllLeftPropertiesStrictEqual(object1, object2) {
+        const json2 = JSON.stringify(object2);
+        for (const key1 in object1) {
+            if (Object.hasOwnProperty.call(object1, key1)) {
+                const value1 = object1[key1];
+                if (Object.hasOwnProperty.call(object2, key1)) {
+                    const value2 = object2[key1];
+                    if (value1 !== value2) {
+                        throw new AssertError(`object1.${key1} === ${value1} !== ${value2} === object1.${key1}`);
+                    }
+                } else {
+                    throw new AssertError(`Object ${json2} is missing the "${key1}" property.`);
+                }
+            }
+        }
+        return true;
+    },
 };
 
 export class AssertError extends Error {
     constructor(...params) {
-      super(...params)
-  
-      // Maintains proper stack trace for where our error was thrown (only available on V8)
-      if (Error.captureStackTrace) {
-        Error.captureStackTrace(this, AssertError);
-      }
-  
-      this.name = "AssertError";
+        super(...params)
+
+        // Maintains proper stack trace for where our error was thrown (only available on V8)
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, AssertError);
+        }
+
+        this.name = "AssertError";
     }
-  }
+}
